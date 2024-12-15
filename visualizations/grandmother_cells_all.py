2db6766e499cc7b75be3ff3380a0c28006a25c24
salihtalha <akgunsalihtalha@gmail.com>
2024-12-15 17:38:00 +0100

    ADDED: results

--- visualizations/grandmother_cells_all.py
+++ visualizations/grandmother_cells_all.py
@@ -103,7 +103,7 @@ def run_all():
 
     tensor = load_data("ankh_merged_tensor.pt")
 
-    for i in tqdm(list(common_labels.keys())[-2:-1]):
+    for i in tqdm(list(common_labels.keys())):
         indexes, titles = [], []
         for j in tqdm(common_labels[i]):
             label_name = i
